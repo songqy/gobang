7343ef937c2c3fee247e2df3a2b957a7408ece54
songqy <songqy3@126.com>
2021-09-09 14:56:30 +0800

    feat: 棋盘

--- src/utils/Gobang.ts
+++ src/utils/Gobang.ts
@@ -1,7 +1,13 @@
 import Drawer from './Drawer';
+import type { PIECE } from './constants';
 
 class Board {
   drawer: Drawer;
+  lines: number;
+  pieces: PIECE[][];
+  space: number;
+  width: number;
+  height: number;
 
   constructor(canvas: HTMLCanvasElement) {
     const ctx = canvas.getContext('2d');
@@ -14,12 +20,24 @@ class Board {
 
       drawer.drawCircle({ x: ev.offsetX, y: ev.offsetY }, 20);
     });
+
+    this.lines = 10;
+    this.space = 30;
+    this.width = 800;
+    this.height = 800;
   }
 
   init(): void {
-    this.drawer.drawLine({ x: 100, y: 100 }, { x: 200, y: 100 });
+    // this.initBoard();
+  }
 
-    this.drawer.drawCircle({ x: 150, y: 300 }, 20);
+  initBoard(): void {
+    for (let i = 0; i < this.lines; ++i) {
+      const offset = (i + 1) * this.space;
+      console.log('offset', offset, this.space);
+      this.drawer.drawLine({ x: 0, y: offset }, { x: this.width, y: offset });
+      this.drawer.drawLine({ x: offset, y: 0 }, { x: offset, y: this.height });
+    }
   }
 }
 
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export enum PIECE {
+  EMPTY,
+  BLACK,
+  WHITE
+}
