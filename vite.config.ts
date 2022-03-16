4ec175906926874f28953eba88ccaac9a5265a68
songqy <songqy3@126.com>
2022-03-16 13:55:01 +0800

    chore: 将yarn改成pnpm，使用dotenv注入环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
+import 'dotenv/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,8 @@ export default defineConfig({
     svelte({
       preprocess: [sveltePreprocess({ typescript: true })]
     })
-  ]
+  ],
+  server:{
+    port:  Number(process.env.PORT) || 3100,
+  },
 });
