568bf9ed44a9585ed18bb1624108573459a81f6e
songqy <songqy3@126.com>
2022-03-16 14:26:38 +0800

    chore: eslint修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,18 +6,31 @@ module.exports = {
   // ignorePatterns: ['*.cjs'],
   overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
   settings: {
-    'svelte3/typescript': () => require('typescript')
+    'svelte3/typescript': () => require('typescript'),
   },
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2019
+    ecmaVersion: 2019,
   },
   env: {
     browser: true,
     es2017: true,
-    node: true
+    node: true,
   },
   rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 0
-  }
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error',  {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'always-multiline',
+      'exports': 'always-multiline',
+      'functions': 'never',
+    }],
+    'indent': ['error', 2],
+    'comma-spacing': 2, //强制在逗号周围使用空格
+    'key-spacing': 2, // 在对象字面量的键和值之间使用一致的空格
+    'keyword-spacing': 2, // 关键字周围空格的一致性
+  },
 };
