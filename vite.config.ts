ad5f2642949796176c6ae21537219717f636da5e
songqy <songqy3@126.com>
2022-03-24 18:56:43 +0800

    style: code style

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
-import 'dotenv/config'
+import 'dotenv/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte({
-      preprocess: [sveltePreprocess({ typescript: true })]
-    })
+      preprocess: [sveltePreprocess({ typescript: true })],
+    }),
   ],
-  server:{
-    port:  Number(process.env.PORT) || 3100,
+  server: {
+    port: Number(process.env.PORT) || 3100,
   },
 });
